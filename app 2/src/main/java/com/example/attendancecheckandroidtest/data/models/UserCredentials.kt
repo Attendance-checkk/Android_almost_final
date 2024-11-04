ec3f08944aefbf25ec978b54faae93d5f2349af7
losiento_nana <edcqazs123@naver.com>
2024-11-04 12:04:21 +0900

    Alarm finish version

--- app 2/src/main/java/com/example/attendancecheckandroidtest/data/models/UserCredentials.kt
+++
@@ -1,8 +0,0 @@
-package com.example.attendancecheckandroidtest.data.models
-
-// 사용자 정보와 JWT 토큰을 포함하는 데이터 모델 클래스
-data class UserCredentials(
-    val userInfo: UserInfo, // 사용자 정보
-    val accessToken: String, // 액세스 토큰
-    val refreshToken: String // 리프레시 토큰
-)
